6cf9a83b37adb65cab95194757e85776252bf826
Bogdan Panteleev <panteleevbbbogdan@gmail.com>
2022-07-16 15:38:39 +0300

    Test async pipe with default change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent{
+  value = {value: 1};
+  log() {
+    console.log('root checked');
+  }
+}
+
+@Component({
+  selector: 'app-nested',
+  template: 'Nested value: {{obs$ | async}} {{log()}}',
+})
+export class NestedComponent{
+  public obs$ = new Observable((subscriber) => {
+    subscriber.next(2);
+    console.log('new subscription');
+  })
+
+  log() {
+    console.log('nested checked');
+  }
 }
